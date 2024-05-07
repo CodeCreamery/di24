85b3bf2de2363780081777e4f6de06442b8c23a3
MilkDevelopment <168671581+MilkDevv@users.noreply.github.com>
2024-05-07 22:05:35 +0200

    made room system, we up

---
+++ public/index.js
@@ -0,0 +1,3 @@
+document.getElementById("createRoom").addEventListener("click", function () {
+	window.location.href = "/new-room"; // Redirect to the server route that creates the room
+});
--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 // Serve static files from 'public' directory
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/new-room", (req, res) => {
+	const roomId = Math.random().toString(36).substring(2, 9); // Create a simple unique ID
+	res.redirect(`/room/${roomId}`); // Redirect user to the room URL
+});
+
+app.get("/room/:roomId", (req, res) => {
+	res.sendFile(__dirname + "/public/room.html");
+});
 
 io.on("connection", (socket) => {
-	console.log(`New user connected: ${socket.id}`);
-
-	// Handling joining a room
-	socket.on("joinRoom", (room) => {
-		socket.join(room);
-		console.log(`User ${socket.id} joined room: ${room}`);
+	socket.on("joinRoom", (roomId) => {
+		socket.join(roomId);
+		console.log(`User joined room: ${roomId}`);
 	});
 
-	// Handling button presses within rooms
-	socket.on("buttonPressed", (data) => {
-		io.to(data.room).emit("updateButtons", data.buttonId);
-		console.log(`Button ${data.buttonId} was pressed in room ${data.room}`);
-	});
-
-	socket.on("disconnect", () => {
-		console.log("User disconnected");
-	});
+	// Further handling for messages or interactions
 });
 
 const PORT = 3000;
